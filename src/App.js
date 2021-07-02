79f1963d0d276765240f42d9bad92ef424a87d74
Aleksa Svilkic <alexa.unity@gmail.com>
2021-07-02 10:34:15 +0200

    Added redux implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from './vendors/redux/slices/counterSlice';
 
 function App() {
+  const dispatch = useDispatch()
+  const counter = useSelector(state => state.counter.value)
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => {dispatch(increment())}}>+</button>
+      <button onClick={() => {dispatch(decrement())}}>-</button>
     </div>
   );
 }
